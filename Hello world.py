f8a0c2964cfdd4d32c3f65b9dd878b629dc4502d
YoussefAhmed973 <youssef.ahmed0games@gmail.com>
2023-06-01 21:34:26 +0300

    Initial commit

--- Hello world.py
+++ Hello world.py
@@ -1,6 +1,22 @@
-user_weight = int(input("Weight: "))
-pounds_or_kilos =  input("(K)g or (L)bs? ")
-if pounds_or_kilos.upper() == "K":
-    print("Wegiht in Pounds is", user_weight*2.222)
-elif pounds_or_kilos.upper() == "L":
-    print("Weight in kilograms is: ", user_weight*0.45)
+#table
+children = range(0, 15)
+youth = range(15, 25)
+adults = range(25, 65)
+seniors = range(65, 10000000000000)
+
+ageInput = int(input("Enter your age! "))
+
+
+
+def determiner():
+    if ageInput in children:
+        print("You have entered", ageInput, "and you are in Children")
+    elif ageInput in youth:
+        print("You have entered", ageInput, "and you are in Youth")
+    elif ageInput in adults:
+        print("You have entered", ageInput,  "and you are in Adult")
+    else:
+        print("You have entered", ageInput, "and you are in Senior")
+
+
+determiner()
